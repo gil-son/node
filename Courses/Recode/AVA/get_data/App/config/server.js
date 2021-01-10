1c75c74a6f42ac041d07ddb0e249747e8f69fe14
Noslig <gilszon@gmail.com>
2021-01-10 10:00:46 -0300

    delete

--- Courses/Recode/AVA/get_data/App/config/server.js
+++
@@ -1,7 +0,0 @@
-/* app contém: mysql, express, commonjs, ejs */
-var express = require('express');
-var app = express();
-
-app.set('view engine','ejs');
-app.set('views','././app/views'); // pasta e path ajustados
-module.exports = app;
