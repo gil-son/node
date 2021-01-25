c9e261ca058149d4865612b2fa42d9d177709ecb
Noslig <gilszon@gmail.com>
2021-01-25 18:25:24 -0300

    Done

--- Courses/Recode/AVA/get_data_and_send_data_2.0/App/config/server.js
+++ Courses/Recode/AVA/get_data_and_send_data_2.0/App/config/server.js
@@ -2,12 +2,14 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var {check, validationResult} = require('express-validator');
+
 
 var app = express();
 app.set('view engine','ejs');
 app.set('views','././app/views'); // pasta e path ajustados
 
-
+// urls codificadas sejam interpretadas via JSON, permitindo uma completa passagem de parâmetros de objetos e matrizes no formato Array.
 app.use(bodyParser.urlencoded({extended:true}));
 
 consign()
--- Courses/Recode/Monitoria/projeto-blog/backend/db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE blog;
-
-CREATE TABLE blog.postagem(
-    id int not null auto_increment primary key,
-    titulo varchar(80),
-    descricao text
-)
