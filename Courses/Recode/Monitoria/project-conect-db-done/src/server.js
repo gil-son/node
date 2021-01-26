a1981d677b63606aeece6dd125b9b39978c69f8e
Noslig <gilszon@gmail.com>
2021-01-25 23:50:15 -0300

    Done

--- Courses/Recode/Monitoria/project-conect-db-done/src/server.js
+++ Courses/Recode/Monitoria/project-conect-db-done/src/server.js
@@ -8,10 +8,42 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"recode_monitoria_node"
+});
+
+
 
 server.get("/usuarios", (req, res) => {
-    res.send("abc");
+    connection.query("SELECT * FROM usuario", (error, result) =>{
+        
+        if(error){
+            res.send(error); // No frontend recupera o error, sem renderizar no backend
+        }else{
+            res.send(result);
+        }
+    });
 });
+
+
+server.post("/usuarios", (req, res) =>{ 
+    const {name} = req.body;
+    res.send(name); // Debug
+    connection.query(`INSERT INTO usuario(name) values('${name}')`, (error, result) =>{
+        if(error){
+            res.send("Erro ao inserir usuário"); 
+        }else{
+            res.status(201).json
+            ({
+                message: "Usuário Cadastrado com Sucesso!"
+            });
+        }
+    });
+});
+
 
 server.listen(3333, () =>{
     console.log("Servidor ativo!");
